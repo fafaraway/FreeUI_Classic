26b974d7dc1cefe6131d5ffd33c6356510197e61
Solor <156806+Solor@users.noreply.github.com>
2019-09-06 12:29:22 +0800

    Cleanup loot strings.

--- FreeUI/scripts/chat/abbreviate.lua
+++ FreeUI/scripts/chat/abbreviate.lua
@@ -112,55 +112,6 @@ function module:Abbreviate()
 	CHAT_YOU_LEFT_NOTICE = '-|Hchannel:%d|h[%s]|h'
 	CHAT_YOU_LEFT_NOTICE_BN = '-|Hchannel:CHANNEL:%d|h[%s]|h'
 
-	CURRENCY_GAINED = '+ %s'
-	CURRENCY_GAINED_MULTIPLE = '+ %sx%d'
-	CURRENCY_GAINED_MULTIPLE_BONUS = '+ %sx%d (Bonus)'
-	CURRENCY_LOST_FROM_DEATH = '- %sx%d'
-
-	LOOT_CURRENCY_REFUND = '+ %sx%d'
-	LOOT_DISENCHANT_CREDIT = '- %s : %s (Disenchant)'
-
-	LOOT_ITEM = '+ %s : %s'
-
-	LOOT_ITEM_BONUS_ROLL = '+ %s : %s (Bonus)'
-	LOOT_ITEM_BONUS_ROLL_MULTIPLE = '+ %s : %sx%d (Bonus)'
-	LOOT_ITEM_BONUS_ROLL_SELF = '+ %s (Bonus)'
-	LOOT_ITEM_BONUS_ROLL_SELF_MULTIPLE = '+ %sx%d (Bonus)'
-
-	LOOT_ITEM_CREATED_SELF = '+ %s (Craft)'
-	LOOT_ITEM_CREATED_SELF_MULTIPLE = '+ %sx%d (Craft)'
-	LOOT_ITEM_CREATED = '+ %s : %s (Craft)'
-	LOOT_ITEM_CREATED_MULTIPLE = '+ %s : %sx%d (Craft)'
-
-	LOOT_ITEM_MULTIPLE = '+ %s : %sx%d'
-	LOOT_ITEM_PUSHED = '+ %s : %s'
-	LOOT_ITEM_PUSHED_MULTIPLE = '+ %s : %sx%d'
-	LOOT_ITEM_PUSHED_SELF = '+ %s'
-	LOOT_ITEM_PUSHED_SELF_MULTIPLE = '+ %sx%d'
-	LOOT_ITEM_REFUND = '+ %s'
-	LOOT_ITEM_REFUND_MULTIPLE = '+ %sx%d'
-	LOOT_ITEM_SELF = '+ %s'
-	LOOT_ITEM_SELF_MULTIPLE = '+ %sx%d'
-	LOOT_ITEM_WHILE_PLAYER_INELIGIBLE = '+ %s : %s'
-
-	LOOT_MONEY = '|cff00a956+|r |cffffffff%s'
-	YOU_LOOT_MONEY = '|cff00a956+|r |cffffffff%s'
-	LOOT_MONEY_SPLIT = '|cff00a956+|r |cffffffff%s'
-	YOU_LOOT_MONEY_GUILD = '|cff00a956+|r |cffffffff%s (%s Guild)'
-
-	COPPER_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-CopperIcon:0:0:2:0\124t'
-	SILVER_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-SilverIcon:0:0:2:0\124t'
-	GOLD_AMOUNT = '%d\124TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0\124t'
-
-	GUILD_NEWS_FORMAT4 = '+ %s : %s (Craft)'
-	GUILD_NEWS_FORMAT8 = '+ %s : %s'
-
-	CREATED_ITEM = '+ %s : %s (Craft)'
-	CREATED_ITEM_MULTIPLE = '+ %s : %sx%d (Craft)'
-
-	TRADESKILL_LOG_FIRSTPERSON = '+ %s : %s (Craft)'
-	TRADESKILL_LOG_THIRDPERSON = '+ %s : %s (Craft)'
-
 	ERR_SKILL_UP_SI = '|cffffffff%s|r |cff00adf0%d|r'
 
 
