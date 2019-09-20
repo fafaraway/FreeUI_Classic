955d4a6e3297b496987034d198e1618b7bb4311e
Solor <156806+Solor@users.noreply.github.com>
2019-09-20 10:04:49 +1000

    Enhance item links on chat frame.

--- FreeUI/scripts/chat/itemlinks.lua
+++ FreeUI/scripts/chat/itemlinks.lua
@@ -2,7 +2,7 @@ local F, C, L = unpack(select(2, ...))
 local CHAT = F:GetModule('Chat')
 
 
--- Show itemlevel on chat hyperlinks
+-- Enhance item links on chat
 local function GetHyperlink(Hyperlink, texture)
 	if (not texture) then
 		return Hyperlink
@@ -24,44 +24,43 @@ local function SetChatLinkIcon(Hyperlink)
 	return GetHyperlink(Hyperlink, texture)
 end
 
-local function isItemHasLevel(link)
-	local name, _, rarity, level, _, _, _, _, _, _, _, classID = GetItemInfo(link)
-	if name and level and rarity > 1 and (classID == LE_ITEM_CLASS_WEAPON or classID == LE_ITEM_CLASS_ARMOR) then
-		local itemLevel = F.GetItemLevel(link)
-		return name, itemLevel
+
+local Caches = {}
+
+local function ChatItemSlot(Hyperlink)
+	if (Caches[Hyperlink]) then
+		return Caches[Hyperlink]
 	end
+	local slot
+	local link = string.match(Hyperlink, '|H(.-)|h')
+	local name, _, quality, level, _, class, subclass, _, equipSlot = GetItemInfo(link)
+	if (equipSlot == 'INVTYPE_CLOAK' or equipSlot == 'INVTYPE_TRINKET' or equipSlot == 'INVTYPE_FINGER' or equipSlot == 'INVTYPE_NECK') then
+		slot = _G[equipSlot] or equipSlot
+	elseif (equipSlot == 'INVTYPE_RANGEDRIGHT') then
+		slot = subclass
+	elseif (equipSlot and string.find(equipSlot, 'INVTYPE_')) then
+		slot = format('%s-%s', subclass or '', _G[equipSlot] or equipSlot)
+	elseif (class == ARMOR) then
+		slot = format('%s-%s', subclass or '', class)
+	elseif (subclass and string.find(subclass, RELICSLOT)) then
+		slot = RELICSLOT
+	elseif (subclass and subclass == MOUNTS) then
+		slot = MOUNTS
+	end
+	if (slot) then
+		Hyperlink = Hyperlink:gsub('|h%[(.-)%]|h', '|h['..slot..': '..name..']|h')
+		Caches[Hyperlink] = Hyperlink
+	end
+	return Hyperlink
 end
 
-local function isItemHasGem(link)
-	local stats = GetItemStats(link)
-	for index in pairs(stats) do
-		if strfind(index, 'EMPTY_SOCKET_') then
-			return '|TInterface\\ItemSocketingFrame\\UI-EmptySocket-Prismatic:0|t'
-		end
-	end
-	return ''
-end
 
-local itemCache = {}
-local function convertItemLevel(link)
-	if itemCache[link] then return itemCache[link] end
-
-	local itemLink = strmatch(link, '|Hitem:.-|h')
-	if itemLink then
-		local name, itemLevel = isItemHasLevel(itemLink)
-		if name and itemLevel then
-			link = gsub(link, '|h%[(.-)%]|h', '|h'..name..' ('..itemLevel..isItemHasGem(itemLink)..')|h')
-			itemCache[link] = link
-		end
-	end
-	return link
-end
-
-function CHAT:UpdateChatItemLevel(_, msg, ...)
-	msg = gsub(msg, '(|Hitem:%d+:.-|h.-|h)', convertItemLevel)
+function CHAT:UpdateChatItemLink(_, msg, ...)
+	msg = gsub(msg, '(|Hitem:%d+:.-|h.-|h)', ChatItemSlot)
 	msg = gsub(msg, '(|H%w+:%d+:.-|h.-|h)', SetChatLinkIcon)
 	return false, msg, ...
 end
+
 
 -- Show item's real color for BN chat
 local queuedMessages = {}
@@ -146,21 +145,19 @@ end)
 function CHAT:ItemLinks()
 	if not C.chat.itemLinks then return end
 
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_LOOT', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_CHANNEL', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_SAY', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_YELL', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_WHISPER', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_WHISPER_INFORM', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_BN_WHISPER', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_RAID', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_RAID_LEADER', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_PARTY', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_PARTY_LEADER', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_GUILD', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_BATTLEGROUND', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_INSTANCE_CHAT', self.UpdateChatItemLevel)
-	ChatFrame_AddMessageEventFilter('CHAT_MSG_INSTANCE_CHAT_LEADER', self.UpdateChatItemLevel)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_LOOT', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_CHANNEL', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_SAY', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_YELL', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_WHISPER', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_WHISPER_INFORM', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_BN_WHISPER', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_RAID', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_RAID_LEADER', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_PARTY', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_PARTY_LEADER', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_GUILD', self.UpdateChatItemLink)
+	ChatFrame_AddMessageEventFilter('CHAT_MSG_BATTLEGROUND', self.UpdateChatItemLink)
 
 	ChatFrame_AddMessageEventFilter('CHAT_MSG_BN_WHISPER', UpdateLinkColor)
 	ChatFrame_AddMessageEventFilter('CHAT_MSG_BN_WHISPER_INFORM', UpdateLinkColor)
